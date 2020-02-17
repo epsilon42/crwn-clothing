45960db2f463eedad1c5795d006f342334304fda
epsilon42 <andrew@goninon.net>
2020-02-17 19:39:20 +0800

    implemented firebase utils, included ability to store authenticated users into firestore db

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -4,7 +4,7 @@ import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
 
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
-  console.log(match);
+  // console.log(match);
   return (
     <div
       className={`${size} menu-item`}
